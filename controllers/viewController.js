5176edf06a9a7dee8a80441d9e2e1ac1a97730d4
vinhthinh11 <vinhthinh1993@gmail.com>
2023-09-14 23:10:30 +0700

    xong video 9

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,14 @@
 const AppError = require('../utils/AppError');
+const Tour = require('../Models/toursSchema');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
-exports.getAllTour = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours' });
-};
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1> Get all tour data from tour collectiong
+  const tours = await Tour.find();
+  // 2> Build tour template
+  // 3> Send back template to client side
+  res.status(200).render('overview', { title: 'All tours', tours });
+});
 exports.getTourDetail = (req, res) => {
   res.status(200).render('tour', { title: 'Tour details' });
 };
